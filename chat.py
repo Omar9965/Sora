4ec5ac444ba5f6648882d8849afc5bb931efce34
Omar <eng16.omarmohamed@gmail.com>
2025-05-01 03:13:52 +0300

    .git/COMMIT_EDITMSG

--- chat.py
+++ chat.py
@@ -82,7 +82,7 @@ async def chat(request: ChatRequest):
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
-    if prob.item() > 0.5:
+    if prob.item() > 0.75:
         for intent in intents['intents']:
             if tag == intent['tag']:
                 return ChatResponse(response=random.choice(intent['responses']))
